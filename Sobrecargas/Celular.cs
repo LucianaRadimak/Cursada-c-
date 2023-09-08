1275c8c4b2eb5570240ab0e918470024d37fcc49
LucianaRadimak <radimakluciana@gmail.com>
2023-09-08 16:45:16 -0300

    CLASE 8/9

---
+++ Sobrecargas/Celular.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecargas
+{
+    internal class Celular
+    {
+        #region Atributos
+        EMarca marca;
+        string modelo;
+        int ram;
+        double almacenamiento;
+        double almacenamientoActual;
+        bool encendido;
+        List<string> agenda;
+        List<string> apps;
+        #endregion
+
+        #region Constructores
+
+        public Celular()
+        {
+            this.apps = new List<string>();
+            this.agenda = new List<string>();
+        }
+
+        public Celular(EMarca marca, string modelo, int ram, double almacenamiento):this()
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.ram = ram;
+            this.almacenamiento = almacenamiento;
+            this.encendido = false;
+            this.almacenamientoActual = almacenamiento;
+        }
+
+        public Celular(EMarca marca, string modelo, int ram, double almacenamiento, List<string> agenda) : this(marca, modelo, ram, almacenamiento)
+        {
+            this.agenda = agenda;
+        }
+        #endregion
+
+        #region Metodos
+        public string AlternarEncendido()
+        {
+            this.encendido = !this.encendido;
+            return this.encendido ? "Encendiendo..." : "Apagando...";
+        }
+        #endregion
+
+    }
+}
---
+++ Sobrecargas/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sobrecargas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> agenda;
+            agenda = new List<String>();
+            agenda.Add("11 12345678");
+            agenda.Add("11 23456789");
+            agenda.Add("11 34567890");
+            Celular celular1;
+            celular1 = new Celular(EMarca.Samsumg, "J7", 8, 64, agenda);
+            Console.WriteLine(celular1.AlternarEncendido());            
+        }
+    }
+}
