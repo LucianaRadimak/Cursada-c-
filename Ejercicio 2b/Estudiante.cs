0e47c03ad57e44038c7fbc563db7321543fc01a7
LucianaRadimak <radimakluciana@gmail.com>
2023-09-08 14:55:42 -0300

    Ejercicio 2b terminado.

---
+++ Ejercicio 2b/Estudiante.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2b
+{
+    internal class Estudiante
+    {
+        string apellido;
+        string legajo;
+        string nombre;
+        int notaPrimerParcial;
+        int notaSegundoParcial;
+        static Random random;
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public int CalcularNotaFinal()
+        {
+            if(notaPrimerParcial > 3 && notaSegundoParcial > 3)
+            {
+                return random.Next(6, 11);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private double CalcularPromedio()
+        {
+            return (notaPrimerParcial + notaSegundoParcial) / 2.0;
+        }
+
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {nombre}");
+            sb.AppendLine($"Apellido: {apellido}");
+            sb.AppendLine($"Legajo: {legajo}");
+            sb.AppendLine($"Nota del primer parcial: {notaPrimerParcial}");
+            sb.AppendLine($"Nota del segundo parcial: {notaSegundoParcial}");
+            double promedio = CalcularPromedio();
+            sb.AppendLine($"Promedio: {promedio}");
+            int notaFinal = CalcularNotaFinal();
+            if (notaFinal != -1)
+            {
+                sb.AppendLine($"Nota Final: {notaFinal}");
+            }
+            else
+            {
+                sb.AppendLine("Alumno desaprobado");
+            }
+            return sb.ToString();                        
+        }
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            notaPrimerParcial = nota;
+        }
+
+        public void SetNotaSegundoParcial(int nota)
+        {
+            notaSegundoParcial = nota;
+        }         
+    }
+}
--- Ejercicio 2b/Program.cs
+++ Ejercicio 2b/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Estudiante estudiante1;
+            estudiante1 = new Estudiante("Isaac", "Newton", "16421727");
+            Estudiante estudiante2;
+            estudiante2 = new Estudiante("Napoleón", "Bonaparte", "17691821");
+            Estudiante estudiante3;
+            estudiante3 = new Estudiante("Albert", "Einstein", "18791955");
+
+            estudiante1.SetNotaPrimerParcial(7);
+            estudiante1.SetNotaSegundoParcial(8);
+
+            estudiante2.SetNotaPrimerParcial(3);
+            estudiante2.SetNotaSegundoParcial(5);
+
+            estudiante3.SetNotaPrimerParcial(9);
+            estudiante3.SetNotaSegundoParcial(10);
+
+            Console.WriteLine("Datos de Estudiante 1:");
+            Console.WriteLine(estudiante1.Mostrar());
+
+            Console.WriteLine("Datos de Estudiante 2:");
+            Console.WriteLine(estudiante2.Mostrar());
+
+            Console.WriteLine("Datos de Estudiante 3:");
+            Console.WriteLine(estudiante3.Mostrar());
         }
     }
 }
